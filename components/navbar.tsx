27335d973ce0a02d93ef94b7d6f7a7be6956b423
m3llo <choojoonlim@gmail.com>
2025-11-05 21:13:17 +0800

    edit navbar content

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,24 +18,16 @@ export default function Navbar() {
                             >
                                 <div className="p-3">
                                     <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">Content Experts</p>
+                                        <p className="font-semibold text-black">The Vivota Oath</p>
                                     </a>
-                                </div>
-                            </PopoverPanel>
-                        </Popover>
-
-                        <Popover>
-                            <PopoverButton className="block text-sm md:text-base font-semibold text-black/50 focus:outline-none data-active:text-black data-focus:outline data-focus:outline-white data-hover:text-black">
-                                What We Offer
-                            </PopoverButton>
-                            <PopoverPanel
-                                transition
-                                anchor="bottom"
-                                className="divide-y divide-white/5 rounded-xl bg-white/90 text-sm/6 transition duration-200 ease-in-out [--anchor-gap:--spacing(5)] data-closed:-translate-y-1 data-closed:opacity-0"
-                            >
-                                <div className="p-3">
+                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
+                                        <p className="font-semibold text-black">Our Content Experts</p>
+                                    </a>
                                     <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
                                         <p className="font-semibold text-black">Vivota House System</p>
+                                    </a>
+                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
+                                        <p className="font-semibold text-black">Student Testimonials</p>
                                     </a>
                                 </div>
                             </PopoverPanel>
@@ -52,16 +44,6 @@ export default function Navbar() {
                             >
                                 <div className="p-3">
                                     <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC General Paper</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC Math</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
                                         <p className="font-semibold text-black">JC Physics</p>
                                     </a>
                                 </div>
@@ -73,6 +55,16 @@ export default function Navbar() {
                                 <div className="p-3">
                                     <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
                                         <p className="font-semibold text-black">JC Biology</p>
+                                    </a>
+                                </div>
+                                <div className="p-3">
+                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
+                                        <p className="font-semibold text-black">JC Math</p>
+                                    </a>
+                                </div>
+                                <div className="p-3">
+                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
+                                        <p className="font-semibold text-black">JC General Paper</p>
                                     </a>
                                 </div>
                             </PopoverPanel>
@@ -82,37 +74,6 @@ export default function Navbar() {
                             <PopoverButton className="block text-sm md:text-base font-semibold text-black/50 focus:outline-none data-active:text-black data-focus:outline data-focus:outline-white data-hover:text-black">
                                 Plans & Pricing
                             </PopoverButton>
-                            <PopoverPanel
-                                transition
-                                anchor="bottom"
-                                className="divide-y divide-white/5 rounded-xl bg-white/90 text-sm/6 transition duration-200 ease-in-out [--anchor-gap:--spacing(5)] data-closed:-translate-y-1 data-closed:opacity-0"
-                            >
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC General Paper</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC Math</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC Physics</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC Chemistry</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC Biology</p>
-                                    </a>
-                                </div>
-                            </PopoverPanel>
                         </Popover>
 
                         <Popover>
@@ -126,16 +87,6 @@ export default function Navbar() {
                             >
                                 <div className="p-3">
                                     <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC General Paper</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
-                                        <p className="font-semibold text-black">JC Math</p>
-                                    </a>
-                                </div>
-                                <div className="p-3">
-                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
                                         <p className="font-semibold text-black">JC Physics</p>
                                     </a>
                                 </div>
@@ -147,6 +98,16 @@ export default function Navbar() {
                                 <div className="p-3">
                                     <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
                                         <p className="font-semibold text-black">JC Biology</p>
+                                    </a>
+                                </div>
+                                <div className="p-3">
+                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
+                                        <p className="font-semibold text-black">JC Math</p>
+                                    </a>
+                                </div>
+                                <div className="p-3">
+                                    <a className="block rounded-lg px-3 py-2 transition hover:bg-white/5" href="#">
+                                        <p className="font-semibold text-black">JC General Paper</p>
                                     </a>
                                 </div>
                             </PopoverPanel>
