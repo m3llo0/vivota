4213fe48055c17f1af46263c9d016b748f7a003c
m3llo <choojoonlim@gmail.com>
2025-11-19 11:37:05 +0800

    adjust favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Cabin } from 'next/font/google';
 import "./globals.css";
 import Footer from "@/components/footer";
 import AnimatedBackground from "@/components/animated_bg";
 import Navbar from "@/components/navbar";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const cabin = Cabin({
   weight: ['400', '700'],
@@ -26,7 +14,17 @@ const cabin = Cabin({
 export const metadata: Metadata = {
   title: "Vivota Education | Founded by Medical Students in Singapore",
   description: `Vivota Education is a trusted tuition centre in Singapore, founded by medical school students who personally tutor. 
-  We offer affordable, high-quality tuition for Junior College students in Chemistry, Physics, Biology, Math, and General Paper to help students excel academically.`,
+  We offer affordable, high-quality tuition for Junior College students in Chemistry, Physics, Biology, Math, and General Paper to
+  help students excel academically.`,
+  icons: {
+    icon: '/favicons/favicon.ico',
+    apple: '/favicons/apple-icon.png',
+    other: [
+      { rel: 'icon', url: '/favicons/web-app-manifest-192x192.png', sizes: '192x192' },
+      { rel: 'icon', url: '/favicons/web-app-manifest-512x512.png', sizes: '512x512' },
+    ],
+  },
+  manifest: '/favicons/manifest.json', 
 };
 
 export default function RootLayout({
