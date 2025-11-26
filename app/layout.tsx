ee87c4d15b6f341cbc3b8214607a7df0c79317f0
m3llo <choojoonlim@gmail.com>
2025-11-26 15:46:55 +0800

    revise sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body
         className={`${cabin.variable} antialiased `}
